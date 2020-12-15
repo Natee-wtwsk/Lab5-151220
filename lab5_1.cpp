1a6bb0f5a463dbbf791d8e0aff61f89b50934614
Natee <gg977120@gmail.com>
2020-12-15 17:26:04 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    int number[] = {};
+    int i = 0;
+
     cout << "Enter an integer: ";
+    cin >> number[i];
+    i++;
+
+    while(number[i]!=0)
+    {
+        cout << "Enter an integer: ";
+        cin >> number[i];
+        cout << i;
+
+        i++;
+    }
+
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
+
     return 0;
 }
